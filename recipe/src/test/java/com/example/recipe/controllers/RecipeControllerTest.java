3d0b08ffe8cc39dc9533ad1b44feaabfb07f3bf6
miguelhuerta <guelme88@gmail.com>
2023-01-01 20:42:15 -0800

    Setup Recipe Controller new method in Service also added unit tests. Template to display data

---
+++ recipe/src/test/java/com/example/recipe/controllers/RecipeControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.recipe.controllers;
+
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.services.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class RecipeControllerTest {
+    RecipeController recipeController;
+
+    @Mock
+    RecipeServiceImpl recipeService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
